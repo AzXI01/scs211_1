5e5ce0ac46d59711403c220639f9592ad91f81b2
AzXI01 <xnxx25371@gmail.com>
2023-08-28 08:06:31 +0700

    1

--- Python/Python.py
+++ Python/Python.py
@@ -2,28 +2,48 @@
 #python <glue.s>
 
 
-##โจทย์ข้อ4 แสดงยอดขายทั้ง2เดือน
+
 #ยากเพราะโง่หรือไม่ได้เรียนก็ไม่รู้
 
 
-# x = สำนักพิมพ์ = (Thairat) (Matichon) (Siam) (Thaipost)
-# result = r
-# มกรายอดขาย = js
-# กุมภายอดขาย = fs
+# x = สำนักพิมพ์ = (Thairat) (Matichon) (Siam) (Thaipost) 
+#c = ยอดขายเดือนมกรา
+#f = ยอดขายเดือนกุมภา
 ##-------------------------------------------------##
-from collections import namedtuple
 
-def demo1():
-    x = (, Matichon, Siam, Thaipost)
-    J = namedtuple ("ยอดขายเดิอนมกรา"["100", "200", "50", "90"])
-    F = namedtuple ("ยอดขายเดิอนกุมภา"["100" , "200" , "50" , "90"])
-T = ('thairat')
-print(T)
-print(J)
-print(F)
+#ข้อ1 ประกาศตัวแปร
+from tkinter.tix import InputOnly
 
-##--------------------------------------------------##
 
+g = ['Thairat', 'Matichon', 'Siam', 'Thaipost']
+
+#ข้อ2 ประกาศตัวแปลเดือนมก
+h = ( '100', '200' , '50' ,'90')
+
+#ข้อ3 ประกาศตัวแปลเดือนกุม
+b = ('120', '250' , '90' ,'100')
+
+##โจทย์ข้อ4 แสดงยอดขายทั้ง2เดือนขอสำนัก thaipost
+print(g[3])
+print("jansold")
+print(h[3])
+print("febsold")
+print(b[3])
+
+#ข้อ5
+#('Newpapers')
+n = []
+a = int
+sum = 0
+while a != 0:
+    a = int(InputOnly('enter num'))
+    n.append(a)
+
+del n[-1]
+cntSum = len(n)
+##-------------------------------------------------##
+#!! Tuple จะเป็น (....)กันสับสน    !!ถ้ามันคำสั่งให้คำใน tupleเปลี่ยนแปลง code จะERR
+#!! list จะเป็น [...] 
 
 
 
